bd1e525d14805b66a2eeceed411f252f74fef5b9
kaurh <kaurh@10.48.4.66>
2019-02-05 20:45:52 -0500

    Coding is modified.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -5,11 +5,15 @@
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha
+ * Modifier - Humpreet Kaur
+ * Student ID - 991516230
  */
 public class CardTrick {
     
@@ -17,16 +21,37 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        Card c = new Card();
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
             //c.setValue(insert call to random number generator here)
+            c.setValue((int)(Math.random() * 13) + 1);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[(int)(Math.random() * 3) + 0]);
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner scan = new Scanner(System.in);
+        System.out.println("User's Card Value and Suit");
+        Card user = new Card();
+        for (int i=0; i<magicHand.length; i++)
+        {
+            user.setValue((int)(Math.random() * 13) + 1);
+            user.setSuit(Card.SUITS[(int)(Math.random() * 3) + 0]);
+        }
+        System.out.println(user.getValue()+ ", " +user.getSuit());
+        
         // and search magicHand here
+        System.out.println("Randomly generated Card Value and Suit");
+        System.out.println(c.getValue()+ ", " +c.getSuit());
+        
         //Then report the result here
+        if(c.getValue()== user.getValue() && c.getSuit().equals(user.getValue())) {
+            System.out.println("Your cards are matched");
+        }
+        else {
+            System.out.println("Your cards are not matched");
+        }
     }
     
 }
