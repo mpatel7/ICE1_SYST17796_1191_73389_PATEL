0549a43d65cfc07f352e9692d551b90b9645dd42
shweta <barot.shweta93@gmail.com>
2019-02-05 20:04:03 -0500

    Add code to get  choose suit and rank from user and give it back.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,6 +11,8 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha
  */
+
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
@@ -20,12 +22,46 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+          //  c.setValue;//(insert call to random number generator here)
+          //  c.setSuit;//(Card.SUITS[insert call to random number between 0-3 here])
         }
         
         //insert code to ask the user for Card value and suit, create their card
+          Scanner reader = new Scanner(System.in); 
+        System.out.print("Kindly Select your Suit from ['Clubs','Diamonds','Hearts','Spades'] ");
+        String suit=reader.nextLine();
+       // Reading from System.in
+        System.out.println("Enter a rank: ");
+        int rank = reader.nextInt();
+
         // and search magicHand here
+        System.out.print("The card you picked is ");
+		switch(rank) // Get rank
+		{
+			case 1: System.out.print("Ace"); break;
+			case 2: System.out.print(rank); break;
+			case 3: System.out.print(rank); break;
+			case 4: System.out.print(rank); break;
+			case 5: System.out.print(rank); break;
+			case 6: System.out.print(rank); break;
+			case 7: System.out.print(rank); break;
+			case 8: System.out.print(rank); break;
+			case 9: System.out.print(rank); break;
+			case 10: System.out.print(rank); break;
+			case 11: System.out.print("Jack"); break;
+			case 12: System.out.print("Queen"); break;
+			case 13: System.out.print("King");
+                         default: System.out.println("No RankHear.");
+		}
+		System.out.print(" of ");
+		switch (suit) // Get suit
+		{
+			case "Clubs": System.out.println("Clubs"); break;
+			case "Diamonds": System.out.println("Diamonds"); break;
+			case "Hearts": System.out.println("Hearts"); break;
+			case "Spades": System.out.println("Spades"); 
+                         default: System.out.println("No suit."); 
+		}
         //Then report the result here
     }
     
