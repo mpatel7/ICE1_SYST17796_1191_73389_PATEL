7e452674523ad408e42cc7c3b52b91bfc0ef32cf
ocojulia <ocojulia@sheridancollege.ca>
2019-02-05 20:46:00 -0500

    Completed CardTrick (with user input)

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,12 +4,14 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha
+ * @modifier Julia991541164
  */
 public class CardTrick {
     
@@ -19,14 +21,34 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            magicHand[i]= new Card();
+            magicHand[i].setValue((int)Math.random() * 13 + 1);
+            magicHand[i].setSuit(Card.SUITS[(int)Math.random() * 3]);
+            //magicHand[i].setSuit(Card.SUITS[3]);
         }
         
         //insert code to ask the user for Card value and suit, create their card
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a card value (1-13): ");
+        int userCardValue = sc.nextInt();
+        System.out.println("Enter a suit (Hearts, Diamonds, Spades, or Clubs: ");
+        sc.next();
+        String userSuit = sc.nextLine();
         // and search magicHand here
+        Boolean withinHand = false;
+	for(Card cardA : magicHand) {
+            if(cardA.getSuit().equalsIgnoreCase(userSuit)){
+                if(cardA.getValue() == userCardValue){
+                    withinHand = true;
+                    break;
+                }
+            }
+            }
+	
         //Then report the result here
+        if(withinHand){
+            System.out.println("Your card is in the hand!");
+        } else System.out.println("Your card is not in the hand.");
     }
     
 }
