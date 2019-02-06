e54f3e105f7d54c353774bb1655f191a7f1fa843
sulocal <sulocal@DESKTOP-U14LNJP>
2019-02-05 21:11:47 -0500

    Made changes in coding

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,7 +11,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha
  * modifier Munmun Nandi
- * Student id 991103144
+ * Student id - 991103144
  */
 import java.util.Scanner;
 public class CardTrick {
