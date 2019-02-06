8e5975a45ca3f1deb1e976bb3bac2627ed73eaa3
suban <shressub@sheridancollege.ca>
2019-02-06 16:31:41 -0500

    changed by Suban

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,9 +1,11 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Modified by : Suban Shrestha
+Student ID : 991527799
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+
+import java.util.HashSet;
+import java.util.*;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -15,12 +17,63 @@ public class CardTrick {
     
     public static void main(String[] args)
     {
+        
+        Scanner in = new Scanner(System.in);
+        
+        
         Card[] magicHand = new Card[7];
         
+       
+                
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
+             
+//c.setValue(insert call to random number generator here)
+             c.setValue((int)(Math.random()*13 +1));
+            
+            c.setSuit(Card.SUITS[(int)(Math.random()*4 )]);
+            
+           
+            magicHand[i] = c;
+             System.out.println("value: "+magicHand[i].getValue()+ magicHand[i].getSuit());
+        }
+        
+        Card userCard = new Card();
+        
+        System.out.print("Enter the card value ");
+        int userValue = in.nextInt();
+        
+        
+        userCard.setValue(userValue);
+        
+        
+        System.out.print("Enter the suit value ");
+        String userSuit = in.next();
+         userCard.setSuit(userSuit);
+         
+         
+         
+           String result ="Cards did not matched";
+             
+        for(int i=0; i<magicHand.length; i++){
+            if(magicHand[i].getSuit().equalsIgnoreCase(userSuit) && magicHand[i].getValue()==userValue){
+           
+                result = "Cards are matched ";
+            
+            break;
+        
+               
+            }
+             
+        }
+         System.out.println(result);
+        
+          
+         
+          
+          
+           
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         
@@ -29,4 +82,4 @@ public class CardTrick {
         //Then report the result here
     }
     
-}
+
