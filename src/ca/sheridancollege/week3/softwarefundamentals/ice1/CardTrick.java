084274c56c95d78cbff0f8e3296cc97367e96215
Lemonade <Lemonade@10.48.56.166>
2019-02-05 20:59:51 -0500

    created a array for storing value in magic hand imported arrays class imported maths class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -11,18 +11,61 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author Megha
  */
+import java.util.*;
+import java.lang.Math;
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Card[] Number=new Card[13];
+        Card[] Suit=new Card[4];
+       int suit;
+       
+       Scanner input=new Scanner(System.in);
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            Number[i]=(int)c.setValue(Math.random()*13+1);
+            Suit[i]=c.getSuit(Math.random()*SUITS.length);
+            
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        System.out.println("Input your card no.");
+        int card_Value = input.nextInt() ;
+        System.out.println("Input your card value");
+        String card_Suit = input.next();
+        
+        if(Arrays.asList(Number).contains(card_Value))
+        {
+            if(Arrays.asList(Suit).contains(card_Suit))
+            {
+                System.out.println("You have same exactly card");
+            }
+            else
+            {
+                System.out.println("Your card have same no.");
+            }
+        }
+        if else(Arrays.asList(Suit).contains(card_Suit))
+                {
+                    if(Arrays.asList(Number).contains(card_Value))
+                    {
+                        System.out.println("You have same exactly card");
+                    }
+                    else
+                    {
+                         System.out.println("Your card have same no.");
+                    }
+                }
+        else
+                {
+                        System.out.println("You do not have same card");
+                }
+                        
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
